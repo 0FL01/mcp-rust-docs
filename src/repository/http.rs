12ed0a2d443a25c65391002269cdeaf16770880b
Ikuma Yamashita <me@ikuma.cloud>
2025-08-15 19:55:44 +0900

    refactor: Simplify error handling in get method of HttpRepositoryImpl

--- src/repository/http.rs
+++ src/repository/http.rs
@@ -11,21 +11,24 @@ impl HttpRepository for HttpRepositoryImpl {
     async fn get(&self, url: &str) -> Result<String, crate::error::Error> {
         let client = crate::cache::get_or_init_reqwest_client().await?;
 
-        let response = client
-            .get(url)
-            .send()
-            .await
-            .map_err(|e| {
-                tracing::error!("{}", e);
-                crate::error::Error::Http(e.to_string())
-            })?
-            .text()
-            .await
-            .map_err(|e| {
-                tracing::error!("{}", e);
-                crate::error::Error::Http(e.to_string())
-            })?;
+        let response = client.get(url).send().await.map_err(|e| {
+            tracing::error!("{}", e);
+            crate::error::Error::Http(e.to_string())
+        })?;
 
-        Ok(response)
+        if !response.status().is_success() {
+            return Err(crate::error::Error::Http(format!(
+                "Failed to fetch URL {}: {}",
+                url,
+                response.status()
+            )));
+        }
+
+        let html = response.text().await.map_err(|e| {
+            tracing::error!("{}", e);
+            crate::error::Error::Http(e.to_string())
+        })?;
+
+        Ok(html)
     }
 }
